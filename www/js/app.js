65c0f160e337d554ad35f3d0d244e4f2efbdcd10
valentin <valentin.debris.1@ens.etsmtl.ca>
2016-03-15 00:35:40 -0400

    -Listing des musiques -Affichage des tirages selon l'évènement choisi -Inscription aux tirages (scan Barcode ou auth via Eventbrite) -Restructuration des fichiers (un dossier par fonctionnalité plutôt que par type)

--- www/js/app.js
+++ www/js/app.js
@@ -1,13 +1,18 @@
-// Ionic Starter App
+// Ionic amc App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'amc' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+// 'amc.controllers' is found in controllers.js
+angular.module('amc', ['ionic','ionic.service.core', 'ionic.service.analytics',
+  'amc.ctrlLayout', 'amc.ctrlSongs', 'amc.ctrlDraws', 'amc.ctrlEvents',
+  'amc.servicesEvents', 'amc.servicesSongs', 'amc.servicesDraws'])
+.constant('URL_WEB_SERVICE', 'http://192.168.1.4:8080/rest/')
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $ionicAnalytics, $cordovaClipboard) {
   $ionicPlatform.ready(function() {
+    //$ionicAnalytics.register();
+
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins.Keyboard) {
@@ -19,55 +24,80 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+
+    //Used for the notifications
+    var push = new Ionic.Push({
+      "debug": true
+    });
+
+    push.register(function(token) {
+      console.log("Device token:",token.token);
+      //alert("Token : "+token.token);
+
+      //$scope.token = token.token;
+      $cordovaClipboard
+        .copy(token.token);
+    });
   });
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
-
-    .state('app', {
+  .state('app', {
     url: '/app',
     abstract: true,
-    templateUrl: 'templates/menu.html',
+    templateUrl: 'layout/menu.html',
     controller: 'AppCtrl'
   })
-
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-
-  .state('app.browse', {
-      url: '/browse',
+    .state('app.events', {
+      url: '/events',
       views: {
         'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
+          templateUrl: 'events/events.html',
+          controller: 'EventsCtrl'
         }
       }
     })
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
+    .state('app.event', {
+      url: '/events/:eventId',
+      views: {
+        'menuContent': {
+          templateUrl: 'events/event.html',
+          controller: 'EventCtrl'
+        }
       }
-    }
-  });
+    })
+
+    .state('app.draws', {
+      url: '/draws',
+      views: {
+        'menuContent': {
+          templateUrl: 'draws/draws.html',
+          controller: 'DrawsCtrl'
+        }
+      }
+    })
+
+    .state('app.draw', {
+      url: '/draws/:drawId',
+      views: {
+        'menuContent': {
+          templateUrl: 'draws/draw.html',
+          controller: 'DrawCtrl'
+        }
+      }
+    })
+
+    .state('app.songs', {
+      url: '/songs',
+      views: {
+        'menuContent': {
+          templateUrl: 'songs/songs.html',
+          controller: 'SongsCtrl'
+        }
+      }
+    });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/events');
 });
